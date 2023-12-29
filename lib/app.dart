e8d5865045f52606806862450c459d786793ba4e
Mahmodul5839 <developer5839@gmail.com>
2023-12-30 01:06:43 +0600

    map_assignment

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Google Map",
-      debugShowCheckedModeBanner: fals,
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
